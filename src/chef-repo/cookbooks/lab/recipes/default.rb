27cc6948fe9624d81075eb9276dc89238a0c42f4
Logan Rakai <lmrakai@gmail.com>
2017-06-24 16:57:17 +0800

    V2: Add cookbook files (web page, Nginx config)

--- src/chef-repo/cookbooks/lab/recipes/default.rb
+++ src/chef-repo/cookbooks/lab/recipes/default.rb
@@ -6,3 +6,13 @@ service 'nginx' do
   supports status: true, restart: true, reload: true
   action [ :enable, :start ]
 end
+
+directory '/var/www'
+
+cookbook_file '/var/www/index.html' do
+ action :create
+end
+
+cookbook_file '/etc/nginx/nginx.conf' do
+ action :create
+end
